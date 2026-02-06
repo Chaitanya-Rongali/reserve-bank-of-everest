da6ad4dbed7ba8001d48ec6c39e6d91835ac4c29
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-06 15:12:45 +0530

    Add server file with basic server setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+const app = express();
+const port = process.env.PORT
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`app listening on ${port} `)
+})
+export default app
