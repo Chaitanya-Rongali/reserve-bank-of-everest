1178d34f84ff804710f303e2256407f9090db1fb
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-08 01:11:51 +0530

    Add update customer deatils functionality

--- src/services/customer.ts
+++ src/services/customer.ts
@@ -1,9 +1,17 @@
 import { customer } from "../models/customer"
-export const createCustomer=async(new_customer:any)=>{
-    const result=await customer.create(new_customer)
+export const createCustomer = async (new_customer: any) => {
+    const result = await customer.create(new_customer)
     return result;
 }
-export const fetchCustomerDetails=async()=>{
-    const result=await customer.findAll();
+export const fetchCustomerDetails = async () => {
+    const result = await customer.findAll();
+    return result;
+}
+export const updateCustomerDetails = async (updateDetails: any, id: string) => {
+    const result = customer.update(updateDetails, {
+        where: {
+            id: id
+        }
+    })
     return result;
 }
