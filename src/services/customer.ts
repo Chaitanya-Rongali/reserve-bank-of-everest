6708ef1dc6fb5cb70196caa4f409496728ba7bc0
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-07 23:54:38 +0530

    Add get customers services functionality

--- src/services/customer.ts
+++ src/services/customer.ts
@@ -3,3 +3,7 @@ export const createCustomer=async(new_customer:any)=>{
     const result=await customer.create(new_customer)
     return result;
 }
+export const fetchCustomerDetails=async()=>{
+    const result=await customer.findAll();
+    return result;
+}
