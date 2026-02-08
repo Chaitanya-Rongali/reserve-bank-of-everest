05f7c260f7b8dfd6b02230b1665f3f11734d41c1
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-09 00:14:23 +0530

    Add transcation to the addacount services functionality

--- src/services/account.ts
+++ src/services/account.ts
@@ -1,11 +1,13 @@
+import { sequelize } from "../config/config";
 import { account } from "../models/account"
 
 export const addAccount = async (account_number: string, account_type: string, balance: number, branch_id: string, id: string) => {
+     const transaction = await sequelize.transaction();
     const result = await account.create({
         account_number: account_number,
         account_type: account_type,
         balance: balance, branch_id,
         customer_id: id
-    })
+    },{transaction})
     return result;
 }
