a9dff4b0565def596507cf98459292f5cafc3525
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-09 01:47:06 +0530

    Add route for blockstatus api

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { addCustomer, deleteCustomer, getCustomersDeatils, modifiyCustomerDetails } from "../controllers/customer";
-import { createAccount } from "../controllers/account";
+import { blockStatus, createAccount } from "../controllers/account";
 export const router=express.Router();
 router.post('/createCustomer',addCustomer)
 router.get('/fetchCustomers',getCustomersDeatils)
 router.put('/updateCustomer/:id',modifiyCustomerDetails)
 router.put('/deleteCustomer/:id',deleteCustomer)
 router.post('/addAccount/:id',createAccount)
+router.put('/blockOrUnblockStatus/:id',blockStatus)
