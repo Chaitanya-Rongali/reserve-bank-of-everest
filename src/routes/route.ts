9c73c246ba393a4c073a1998f3a26e2d7c9e9851
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-08 21:34:41 +0530

    Add route for create account

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import { addCustomer, deleteCustomer, getCustomersDeatils, modifiyCustomerDetails } from "../controllers/customer";
+import { createAccount } from "../controllers/account";
 export const router=express.Router();
 router.post('/createCustomer',addCustomer)
 router.get('/fetchCustomers',getCustomersDeatils)
 router.put('/updateCustomer/:id',modifiyCustomerDetails)
 router.put('/deleteCustomer/:id',deleteCustomer)
+router.post('/addAccount/:id',createAccount)
