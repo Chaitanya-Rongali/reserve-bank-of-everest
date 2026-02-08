01c78e68ffed35d3ae9b2d8b6013601532c0b9f3
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-09 00:11:18 +0530

    Add audit logs migration file

---
+++ migrations/20260208170741-create-audit-log.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('AuditLogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      table_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      operation_type: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      before_status: {
+        allowNull: false,
+        type: Sequelize.JSON
+      },
+      after_status: {
+        allowNull: false,
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('AuditLogs');
+  }
+};
