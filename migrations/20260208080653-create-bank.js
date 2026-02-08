7a71185cbe30af9413a93c4ca1e97dd14bd30508
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-08 15:17:23 +0530

    Add bank migration file for store bank details

---
+++ migrations/20260208080653-create-bank.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('banks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      bank_code: {
+        type: Sequelize.STRING,
+        unique:true,
+        allowNull:false
+      },
+      Name: {
+        type: Sequelize.STRING,
+        unique:true,
+        allowNull:false
+      },
+      Address: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('banks');
+  }
+};
