2d962edc5a87e95f3a930b822ff69a897cd0a85c
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-09 17:46:50 +0530

    Add trasncations controller functionality

---
+++ src/controllers/balancetransactions.ts
@@ -0,0 +1,48 @@
+import { sequelize } from "../config/config";
+import { Request, Response } from "express";
+import { account } from "../models/account";
+import { transactionsServices } from "../services/balancetransaction";
+import { auditlogs } from "../models/auditlogs";
+
+export const transactions = async (req: Request, res: Response) => {
+    const transaction = await sequelize.transaction();
+    try {
+        const { sendAccountId, recevierAccountId, balance } = req.body
+        const receivingCustomer: any = await account.findOne({
+            where: { id: recevierAccountId },
+            lock: transaction.LOCK.UPDATE,
+            transaction
+        })
+        const sendingCustomer: any = await account.findOne({
+            where: { id: sendAccountId },
+            lock: transaction.LOCK.UPDATE,
+            transaction
+        })
+        if (sendingCustomer?.dataValues.balance < balance) {
+            throw new Error('Insuffcient balance')
+        }
+        if (!receivingCustomer || !sendingCustomer) {
+            throw new Error('Account not found');
+        }
+        if (balance < 0) {
+            throw new Error('balance must be postive');
+        }
+        const prevSendingCustomer = sendingCustomer.dataValues;
+        const prevReceivingCustomer = receivingCustomer.dataValues;
+        const prevState = { prevSendingCustomer, prevReceivingCustomer }
+        const result = await transactionsServices(receivingCustomer, sendingCustomer, balance, transaction)
+        await auditlogs.create({
+            url: req.originalUrl,
+            table_name: account.tableName,
+            operation_type: "Transfer",
+            before_status:prevState,
+            after_status:result,
+        }, { transaction })
+        await transaction.commit();
+        return res.send({ message: `successfully ${balance} rupees to the ${recevierAccountId}` })
+    } catch (error: any) {
+        await transaction.rollback();
+        return res.status(500).send(error.message)
+    }
+}
+
