6be6a600ff67189c4d7aa25134eff2c64f896973
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-10 11:27:04 +0530

    Modifiy the routes with separate separte files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from 'dotenv'
-import { router } from "./routes/route";
+import { customerRouter } from "./routes/customerroute";
+import { accountRouter } from "./routes/accountroute";
+import { transactionRouter } from "./routes/transcationroute";
 dotenv.config({ path: '.env' })
 const app = express();
 const port = process.env.PORT
 app.use(express.json());
-app.use('/api',router)
+app.use('/api',customerRouter)
+app.use('/api',accountRouter)
+app.use('/api',transactionRouter)
 app.listen(port, () => {
     console.log(`app listening on ${port} `)
 })
